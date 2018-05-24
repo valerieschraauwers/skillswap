9a1bb3814dfa0436bcd76762357f927adb8b8590
valerie schraauwers <valerieschraauwers@gmail.com>
2018-05-24 14:57:06 +0800

    Added userskillscontroller, and activated figaro - Add figaro - Protect my secret data in config/application.yml file

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,12 +2,10 @@ class SkillsController < ApplicationController
 
   def learn
     @categories = SkillCategory.all
-    puts "We are in the learn selection"
   end
 
   def teach
     @categories = SkillCategory.all
-    puts "test we are in the teach selection"
   end
 
   def show
---
+++ app/controllers/user_skills_controller.rb
@@ -0,0 +1,4 @@
+class UserSkillsController < ApplicationController
+
+
+end
