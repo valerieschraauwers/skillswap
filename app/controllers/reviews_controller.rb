2fbd727e7cd18b5481d2985a8ceb892aaaee5b96
WIlliam Calderon <wec999@gmail.com>
2018-05-25 19:36:24 +0800

    Review working with Ajax

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  def create
+    @match = Match.find(params[:match_id])
+    @review = Review.new(review_params)
+    @review.match = @match
+    @review.user = current_user
+    if @review.save
+      respond_to do |format|
+        format.html { redirect_to match_path(@match) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'matches/show' }
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
