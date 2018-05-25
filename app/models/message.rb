13b5a0d6e7039314c165c62b603dc0e16120ee4b
WIlliam Calderon <wec999@gmail.com>
2018-05-25 12:25:06 +0800

    Basic chat added

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
-class Message < ApplicationRecord
-  belongs_to :user
-  belongs_to :match
+class Message < ActiveRecord::Base
+ belongs_to :conversation
+ belongs_to :user
+ # belongs_to :match
+
+ validates_presence_of :content, :conversation_id, :user_id
+
+ def message_time
+  created_at.strftime("%m/%d/%y at %l:%M %p")
+ end
+
 end
