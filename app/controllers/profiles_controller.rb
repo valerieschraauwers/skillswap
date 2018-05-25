a27c1c8cf214326dba6b097534bd92d168a2f2b7
valerie schraauwers <valerieschraauwers@gmail.com>
2018-05-25 15:34:48 +0800

    Added edit profile form to the last step of the signup process

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,7 @@
 class ProfilesController < ApplicationController
 
   def show
-    # show individual profile or my profile? or can we define that in the html which we want to see?
+    # show individual profile
   end
 
-  def edit
-    # user = current_user
-    # learn_skills: all the skills selected will have default value learn
-    # teach_skills: all the skills selected will have default value teach
-    # User.update?
-    # redirect to page 2 > select skills to teach
-  end
-
-  def update
-
-  end
 end
