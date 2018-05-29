495bd591ab5d4d4ff8dad7054184d19c917926b4
WIlliam Calderon <wec999@gmail.com>
2018-05-29 14:43:56 +0800

    Create uniqueness validations for user_skills and matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,6 +3,8 @@ class Match < ApplicationRecord
   belongs_to :student_skill, class_name: 'UserSkill', foreign_key: 'student_skill_id'
   has_many :reviews, dependent: :destroy
 
+  validates :teacher_skill_id, uniqueness: { scope: :student_skill_id}
+
   def teacher
     UserSkill.find(self.teacher_skill_id).user
   end
