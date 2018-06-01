303ea5bae639798b856c35db36099f4beb04f6c1
valerie schraauwers <valerieschraauwers@gmail.com>
2018-06-01 17:05:38 +0800

    Changes made before heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,7 +101,7 @@ Skill.create(name: "Baking", skill_category_id: 3, icon: "icons/food_and_drink/b
 Skill.create(name: "Coffee", skill_category_id: 3, icon: "icons/food_and_drink/coffee.svg")
 Skill.create(name: "Cooking", skill_category_id: 3, icon: "icons/food_and_drink/cooking.svg")
 Skill.create(name: "French Cooking", skill_category_id: 3, icon: "icons/food_and_drink/french_cooking.svg")
-Skill.create(name: "Mexican Cooking", skill_category_id: 3, icon: "icons/food_and_drink/mexicanhappytaco.svg")
+# Skill.create(name: "Mexican Cooking", skill_category_id: 3, icon: "icons/food_and_drink/mexicanhappytaco.svg")
 Skill.create(name: "Thai Cooking", skill_category_id: 3, icon: "icons/food_and_drink/thai_cooking.svg")
 Skill.create(name: "Vietnamese", skill_category_id: 3, icon: "icons/food_and_drink/vietnamese_cooking.svg")
 Skill.create(name: "Wine Tasting", skill_category_id: 3, icon: "icons/food_and_drink/winetasting.svg")
@@ -204,7 +204,7 @@ UserSkill.create(user_id: 3, skill_id: 24, role: "student") #VALERIE STUDENT
 UserSkill.create(user_id: 5, skill_id: 24, role: "teacher") #TEACHER
 
 UserSkill.create(user_id: 6, skill_id: 21, role: "student") #STUDENT
-UserSkill.create(user_id: 3, skill_id: 21 role: "teacher") #VALERIE TEACHER
+UserSkill.create(user_id: 3, skill_id: 21, role: "teacher") #VALERIE TEACHER
 
 UserSkill.create(user_id: 3, skill_id: 20, role: "student") #VALERIE STUDENT
 UserSkill.create(user_id: 9, skill_id: 20, role: "teacher") #TEACHER
@@ -234,7 +234,7 @@ UserSkill.create(user_id: 4, skill_id: 50, role: "student") #STUDENT
 UserSkill.create(user_id: 3, skill_id: 50, role: "teacher") #VALERIE TEACHER
 
 UserSkill.create(user_id: 3, skill_id: 48, role: "student") #VALERIE STUDENT
-UserSkill.create(user_id: 7, skill_id: 48 role: "teacher") #TEACHER
+UserSkill.create(user_id: 7, skill_id: 48, role: "teacher") #TEACHER
 
 UserSkill.create(user_id: 8, skill_id: 46, role: "student") #STUDENT
 UserSkill.create(user_id: 3, skill_id: 46, role: "teacher") #VALERIE TEACHER
@@ -258,13 +258,13 @@ Match.create(teacher_skill_id: 10, student_skill_id: 23)
 Match.create(teacher_skill_id: 11, student_skill_id: 21)
 Match.create(teacher_skill_id: 12, student_skill_id: 22)
 Match.create(teacher_skill_id: 13, student_skill_id: 20)
-Match.create(teacher_skill_id: 14, student_skill_id: 19)
-Match.create(teacher_skill_id: 15, student_skill_id: 18)
-Match.create(teacher_skill_id: 16, student_skill_id: 17)
-Match.create(teacher_skill_id: 17, student_skill_id: 16)
-Match.create(teacher_skill_id: 18, student_skill_id: 15)
-Match.create(teacher_skill_id: 19, student_skill_id: 14)
-Match.create(teacher_skill_id: 20, student_skill_id: 13)
+# Match.create(teacher_skill_id: 14, student_skill_id: 19)
+# Match.create(teacher_skill_id: 15, student_skill_id: 18)
+# Match.create(teacher_skill_id: 16, student_skill_id: 17)
+# Match.create(teacher_skill_id: 17, student_skill_id: 16)
+# Match.create(teacher_skill_id: 18, student_skill_id: 15)
+# Match.create(teacher_skill_id: 19, student_skill_id: 14)
+# Match.create(teacher_skill_id: 20, student_skill_id: 13)
 Match.create(teacher_skill_id: 21, student_skill_id: 12)
 Match.create(teacher_skill_id: 22, student_skill_id: 11)
 Match.create(teacher_skill_id: 23, student_skill_id: 10)
